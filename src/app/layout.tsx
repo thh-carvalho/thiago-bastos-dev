0de4c1cbd4856cef1b27d5db7596d291a7690a0f
Thiago H. Carvalho Bastos <47954204+thh-carvalho@users.noreply.github.com>
2024-10-25 03:59:20 -0300

    Ajustando meta tags

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,13 +10,29 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Thiago Bastos",
   description: "Desenvolvedor Front-end & Web Analytics",
+  icons: [
+    {
+      rel: 'apple-touch-icon',
+      sizes: '180x180',
+      url: '/layout/favicon/apple-touch-icon.png',
+    },
+    {
+      rel: 'icon',
+      type: 'image/png',
+      sizes: '32x32',
+      url: '/static/thiago.png',
+    },
+    {
+      rel: 'shortcut icon',
+      url: '/static/thiago.png',
+    },
+  ],
 };
 
 export default function RootLayout({ children, }: { children: React.ReactNode}) {
   return (
     <html lang="pt-br">
       <body className="bg-[url('https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExMWxmajZoYTBvbmU4c2FneG44Znd1eXI2ZGl6NzgxcXRubDlweDJxcSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/U4ExkAvRpVQGB0NMe0/giphy.webp')] bg-no-repeat bg-cover">
-        <link rel="shortcut icon" href="/static/thiago.png" />
         <div className="grid grid-cols-2 max-[990px]:flex max-[990px]:flex-col max-[990px]:justify-start">
           <div>
             <Hero />
--- src/app/pages/_document.tsx
+++ src/app/pages/_document.tsx
@@ -1,44 +1,29 @@
-import Document, {
-    Html,
-    Head,
-    Main,
-    NextScript,
-    DocumentContext,
-    DocumentInitialProps,
-  } from 'next/document'
-   
-  class MyDocument extends Document {
-    static async getInitialProps(
-      ctx: DocumentContext
-    ): Promise<DocumentInitialProps> {
-      const originalRenderPage = ctx.renderPage
-   
-      // Run the React rendering logic synchronously
-      ctx.renderPage = () =>
-        originalRenderPage({
-          // Useful for wrapping the whole react tree
-          enhanceApp: (App) => App,
-          // Useful for wrapping in a per-page basis
-          enhanceComponent: (Component) => Component,
-        })
-   
-      // Run the parent `getInitialProps`, it now includes the custom `renderPage`
-      const initialProps = await Document.getInitialProps(ctx)
-   
-      return initialProps
-    }
-   
-    render() {
-      return (
-        <Html lang="en">
-          <Head />
-          <body>
-            <Main />
-            <NextScript />
-          </body>
-        </Html>
-      )
-    }
-  }
-   
-  export default MyDocument
+import { Html, Head, Main, NextScript } from "next/document"
+
+export default function Document() {
+  return (
+    <Html lang='pt-br'>
+      <Head>
+        <link
+          rel='apple-touch-icon'
+          sizes='180x180'
+          href='/layout/favicon/apple-touch-icon.png'
+        />
+        <link
+            rel='icon'
+            type='image/png'
+            sizes='16x16'
+            href="/static/thiago.png" />
+        <meta
+          name='msapplication-config'
+          content='/layout/favicon/browserconfig.xml'
+        />
+      </Head>
+
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
