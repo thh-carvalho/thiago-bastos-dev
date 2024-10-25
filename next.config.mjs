251d54c61e471d0915087fc44fcf1851a4f33591
Thiago H. Carvalho Bastos <47954204+thh-carvalho@users.noreply.github.com>
2024-10-25 05:12:16 -0300

    Deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // basePath: "/thiago-bastos-dev",
-    // output: "export",  // <=== enables static exports
-    // reactStrictMode: true,
+    basePath: "/thiago-bastos-dev",
+    output: "export",  // <=== enables static exports
+    reactStrictMode: true,
 };
 
 export default nextConfig;
