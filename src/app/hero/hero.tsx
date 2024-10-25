27ef82cbf10681e8a110b160a2993c8ff284973f
Thiago H. Carvalho Bastos <47954204+thh-carvalho@users.noreply.github.com>
2024-10-25 06:00:00 -0300

    Configurando eventos de click nos ícones do hero

--- src/app/hero/hero.tsx
+++ src/app/hero/hero.tsx
@@ -1,9 +1,10 @@
 'use client'
  
 import { usePathname } from 'next/navigation'
-import Link from 'next/link'
+import { sendGTMEvent } from '@next/third-parties/google'
 import { GitHub } from '@mui/icons-material'
 import { LinkedIn } from '@mui/icons-material'
+import { Email } from '@mui/icons-material'
  
 export function Hero() {
   const pathname = usePathname()
@@ -21,18 +22,21 @@ export function Hero() {
         <div className='py-9'>
             <ul>
                 <a href="#about"><li className="flex items-center text-slate-400 cursor-pointer py-3"><div className='w-14 h-0 border-t-2 border-slate-500 mr-2'></div>SOBRE</li></a>
-                <a href="#experience"><li className="flex items-center text-slate-500 cursor-pointer py-3"><div className='w-10 h-0 border-t-2 border-slate-600 mr-2'></div>EXPERIÊNCIA</li></a>
-                <a href="#projects"><li className="flex items-center text-slate-500 cursor-pointer py-3"><div className='w-10 h-0 border-t-2 border-slate-600 mr-2'></div>PROJETOS</li></a>
+                <a href="#experience"><li className="flex items-center text-slate-500 cursor-pointer py-3 hover:text-slate-400"><div className='w-10 h-0 border-t-2 border-slate-600 mr-2'></div>EXPERIÊNCIA</li></a>
+                <a href="#projects"><li className="flex items-center text-slate-500 cursor-pointer py-3 hover:text-slate-400"><div className='w-10 h-0 border-t-2 border-slate-600 mr-2'></div>PROJETOS</li></a>
             </ul>
         </div>
 
         <div className='py-3'>
             <ul className='flex'>
-                <li className="text-slate-600 pr-3">
+                <li className="text-slate-600 pr-3 hover:text-slate-400" onClick={() => sendGTMEvent({ event: 'click', event_label: 'click:github', event_category:'github', event_action:'click:github' })}>
                     <a href="https://github.com/thh-carvalho" target='_blank'><GitHub fontSize="large"/></a>                    
                 </li>
-                <li className="text-slate-600">
+                <li className="text-slate-600 pr-3 hover:text-slate-400" onClick={() => sendGTMEvent({ event: 'click', event_label: 'click:linkedin', event_category:'linkedin', event_action:'click:linkedin' })}>
                     <a href="https://www.linkedin.com/in/thiagohcarvalho/" target='_blank'><LinkedIn fontSize="large"/></a>                    
+                </li>
+                <li className="text-slate-600 pr-3 hover:text-slate-400" onClick={() => sendGTMEvent({ event: 'click', event_label: 'click:email', event_category:'email', event_action:'click:email' })}>
+                    <a href="mailto:thiago.bastosdev@gmail.com" target='_blank'><Email fontSize="large"/></a>                    
                 </li>
             </ul>
         </div>
