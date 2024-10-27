abb81581c5a0dd6dc6070a7de8fef6ca60a109de
Thiago H. Carvalho Bastos <47954204+thh-carvalho@users.noreply.github.com>
2024-10-27 06:38:29 -0300

    update

--- src/app/footer/footer.tsx
+++ src/app/footer/footer.tsx
@@ -10,7 +10,7 @@ export function Footer() {
  
   return (
     <section id='projects' className='py-10 pr-20 max-[990px]:py-18 max-[990px]:px-7'>
-        <a className='bg-teal-800 text-teal-100 p-2 px-5 mb-9 inline-block rounded-md' href="https://wa.me/11998080897?text=Olá%20Thiago,%20gostaria%20de%20saber%20mais%20sobre%20seu%20trabalho" target='_blank' onClick={() => sendGTMEvent({ event: 'click_cta', event_label: 'cta:entrar-em-contato', event_category:'cta:click', event_action:'click:entrar-em-contato' })}>Entrar em contato <ArrowForward/></a>
+        <a className='bg-teal-800 text-teal-100 p-2 px-5 mb-9 inline-block rounded-md' href="https://wa.me/+5511998080897?text=Olá%20Thiago,%20gostaria%20de%20saber%20mais%20sobre%20seu%20trabalho" target='_blank' onClick={() => sendGTMEvent({ event: 'click_cta', event_label: 'cta:entrar-em-contato', event_category:'cta:click', event_action:'click:entrar-em-contato' })}>Entrar em contato <ArrowForward/></a>
         <p className='text-slate-300'>Site desenvolvido com Next.js & Tailwind por Thiago Bastos</p>
     </section>
   )
